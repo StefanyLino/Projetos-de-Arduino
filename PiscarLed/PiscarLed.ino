ca0b0649b6f87fd37522d74667329e6c8f6c774e
Stefany Lino <stefanylinosilva47@gmail.com>
2024-08-05 15:20:35 -0300

    código identado

--- PiscarLed/PiscarLed.ino
+++ PiscarLed/PiscarLed.ino
@@ -1,12 +1,11 @@
+//Programa para piscar o led
 void setup() {
-
-pinMode(10,OUTPUT);
-
+  pinMode(10,OUTPUT); //define a porta do led e como saída
 }
 
 void loop() {
-  digitalWrite(10,HIGH);
-  delay(2000);
-  digitalWrite(10,LOW);
+  digitalWrite(10,HIGH); //define a porta como alta/baixa, faz o led ligar
+  delay(2000); //temporizador de 2 segundos
+  digitalWrite(10,LOW); //define a porta como alta/baixa, faz o led desligar
   delay(2000);
 }
